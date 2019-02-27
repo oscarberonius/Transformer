117066d55546b3b2c1590cac53b34af66effe1a1
oscarberonius <oscar.beronius@gmail.com>
2019-02-27 18:25:21 +0100

    Experiment: Include validation loss during training.

--- train.py
+++ train.py
@@ -19,6 +19,7 @@ def train_model(model, opt):
     for epoch in range(opt.epochs):
 
         total_loss = 0
+        val_loss = 0
         if opt.floyd is False:
             print("   %dm: epoch %d [%s]  %d%%  loss = %s" %\
             ((time.time() - start)//60, epoch + 1, "".join(' '*20), 0, '...'), end='\r')
@@ -57,11 +58,46 @@ def train_model(model, opt):
             if opt.checkpoint > 0 and ((time.time()-cptime)//60) // opt.checkpoint >= 1:
                 torch.save(model.state_dict(), 'weights/model_weights')
                 cptime = time.time()
+
+        for i, batch in enumerate(opt.val):
+            
+            src = batch.src.transpose(0,1).cuda()
+            trg = batch.trg.transpose(0,1).cuda()
+            trg_input = trg[:, :-1]
+            src_mask, trg_mask = create_masks(src, trg_input, opt)
+            preds = model(src, trg_input, src_mask, trg_mask)
+            ys = trg[:, 1:].contiguous().view(-1)
+            # opt.optimizer.zero_grad()
+            loss = F.cross_entropy(preds.view(-1, preds.size(-1)), ys, ignore_index=opt.trg_pad)
+            # loss.backward()
+            # opt.optimizer.step()
+            # if opt.SGDR == True: 
+            #     opt.sched.step()
+            
+            val_loss += loss.item()
+            
+            if (i + 1) % opt.printevery == 0:
+                 p = int(100 * (i + 1) / opt.val_len)
+                 avg_val_loss = val_loss/opt.printevery
+                 if opt.floyd is False:
+                    print("   %dm: epoch %d [%s%s]  %d%%  loss = %.3f" %\
+                    ((time.time() - start)//60, epoch + 1, "".join('#'*(p//5)), "".join(' '*(20-(p//5))), p, avg_val_loss), end='\r')
+                 else:
+                    print("   %dm: epoch %d [%s%s]  %d%%  loss = %.3f" %\
+                    ((time.time() - start)//60, epoch + 1, "".join('#'*(p//5)), "".join(' '*(20-(p//5))), p, avg_val_loss))
+                 val_loss = 0
+            
+            if opt.checkpoint > 0 and ((time.time()-cptime)//60) // opt.checkpoint >= 1:
+                torch.save(model.state_dict(), 'weights/model_weights')
+                cptime = time.time()
    
    
         print("%dm: epoch %d [%s%s]  %d%%  loss = %.3f\nepoch %d complete, loss = %.03f" %\
         ((time.time() - start)//60, epoch + 1, "".join('#'*(100//5)), "".join(' '*(20-(100//5))), 100, avg_loss, epoch + 1, avg_loss))
 
+        print("%dm: epoch %d [%s%s]  %d%%  loss = %.3f\nepoch %d complete, loss = %.03f" %\
+        ((time.time() - start)//60, epoch + 1, "".join('#'*(100//5)), "".join(' '*(20-(100//5))), 100, avg_val_loss, epoch + 1, avg_val_loss))
+        
 def main():
 
     parser = argparse.ArgumentParser()
