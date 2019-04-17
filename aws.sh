188f3be3e4a4e1e3bc6d6a3d890238559ae13d03
oscarberonius <oscar.beronius@gmail.com>
2019-04-17 13:53:15 +0000

    Add structure experiment haxx0r script.

--- aws.sh
+++ aws.sh
@@ -10,3 +10,18 @@ download(){
 aws_ls(){
     aws s3 ls s3://oscar-multi-doc-summarizer-thesis/
 }
+
+structure_experiment(){
+    mv raw_train.json raw_train_$1.json
+    mv raw_val.json raw_val_$1.json
+    mv train_loss.pickle train_loss_$1.pickle
+    mv val_loss.pickle val_loss_$1.pickle
+    mv weights_latest weights_$1
+    upload train_loss_$1.pickle
+    upload val_loss_$1.pickle
+    mv raw_train_$1.json raw
+    mv raw_val_$1.json raw
+    mv train_loss_$1.pickle pickles
+    mv val_loss_$1.pickle pickles
+    mv weights_$1 weights
+}
